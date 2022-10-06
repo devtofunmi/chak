692687dd3142af5a86d1af306fa40a613f92a9ca
Dev Tofunmi <olayiwolajesutofunmi@gmail.com>
2022-10-06 22:03:39 +0100

    yea

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import SignUp from "../components/SignUp";
+import Login from "../components/Login";
+import Otp from "../components/Otp";
+import Profile from "../components/Profile";
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <SignUp />
+    </div>
+  );
 };
 
 export default Homepage;
